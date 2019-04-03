3ac92994e5d0c4e99c24c35cd7fb62cb22d33b26
unknown <CGuijarro>
2019-04-03 16:23:00 -0500

    tururu

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>b15c92e9-1835-4d33-89d2-446fe5bfc681</testSuiteGuid>
+   <testCaseLink>
+      <guid>c87cd5cb-e99d-4676-8fba-bd797aba89fc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test - 1 By Tom</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>55120a5e-10d7-4f5e-9d00-d3539c5f3856</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case 991</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
